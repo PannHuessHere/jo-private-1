df9782cfde5d511b85b0d27cf2d0b90f948a9dae
PannHuessHere <113444188+PannHuessHere@users.noreply.github.com>
2022-10-08 02:40:12 +0700

    Create config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'IFMeans MODS'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281717307804','6281717307804','6281717307804']
+global.premium = ['6281717307804']
+global.packname = 'IFMeans MODS'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
